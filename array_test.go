616cb8a94913d7a67fb94dd4e1c65a0d864560b1
Faye Amacker <33205765+fxamacker@users.noreply.github.com>
2021-06-17 09:48:17 -0500

    Add Remove(), Merge(), and Rebalance()

--- array_test.go
+++ array_test.go
@@ -22,6 +22,10 @@ func TestAppendAndGet(t *testing.T) {
 		require.NoError(t, err)
 		require.Equal(t, i, e)
 	}
+
+	verified, err := array.valid()
+	require.NoError(t, err)
+	require.True(t, verified)
 }
 
 func TestInsertAndGet(t *testing.T) {
@@ -48,6 +52,10 @@ func TestInsertAndGet(t *testing.T) {
 			require.NoError(t, err)
 			require.Equal(t, i, e)
 		}
+
+		verified, err := array.valid()
+		require.NoError(t, err)
+		require.True(t, verified)
 	})
 
 	t.Run("insert-last", func(t *testing.T) {
@@ -71,6 +79,10 @@ func TestInsertAndGet(t *testing.T) {
 			require.NoError(t, err)
 			require.Equal(t, i, e)
 		}
+
+		verified, err := array.valid()
+		require.NoError(t, err)
+		require.True(t, verified)
 	})
 
 	t.Run("insert", func(t *testing.T) {
@@ -99,39 +111,175 @@ func TestInsertAndGet(t *testing.T) {
 			require.NoError(t, err)
 			require.Equal(t, i, e)
 		}
+
+		verified, err := array.valid()
+		require.NoError(t, err)
+		require.True(t, verified)
+	})
+}
+
+func TestRemove(t *testing.T) {
+	t.Parallel()
+
+	t.Run("remove-first", func(t *testing.T) {
+		setThreshold(50)
+		defer func() {
+			setThreshold(1024)
+		}()
+
+		storage := NewBasicStorage()
+
+		array := NewArray(storage)
+
+		n := uint64(256 * 256)
+		for i := uint64(0); i < n; i++ {
+			err := array.Append(i)
+			require.NoError(t, err)
+		}
+
+		require.Equal(t, n, array.Count())
+
+		for i := uint64(0); i < n; i++ {
+			v, err := array.Remove(0)
+			require.NoError(t, err)
+			require.Equal(t, i, v)
+			require.Equal(t, n-i-1, array.Count())
+
+			if i%8 == 0 {
+				verified, err := array.valid()
+				require.NoError(t, err)
+				require.True(t, verified)
+			}
+		}
+
+		require.Equal(t, uint64(0), array.Count())
+	})
+
+	t.Run("remove-last", func(t *testing.T) {
+		setThreshold(50)
+		defer func() {
+			setThreshold(1024)
+		}()
+
+		storage := NewBasicStorage()
+
+		array := NewArray(storage)
+
+		n := uint64(256 * 256)
+		for i := uint64(0); i < n; i++ {
+			err := array.Append(i)
+			require.NoError(t, err)
+		}
+
+		require.Equal(t, n, array.Count())
+
+		for i := int(n) - 1; i >= 0; i-- {
+			v, err := array.Remove(uint64(i))
+			require.NoError(t, err)
+			require.Equal(t, uint64(i), v)
+			require.Equal(t, uint64(i), array.Count())
+
+			if i%8 == 0 {
+				verified, err := array.valid()
+				require.NoError(t, err)
+				require.True(t, verified)
+			}
+		}
+
+		require.Equal(t, uint64(0), array.Count())
+	})
+
+	t.Run("remove", func(t *testing.T) {
+		setThreshold(50)
+		defer func() {
+			setThreshold(1024)
+		}()
+
+		storage := NewBasicStorage()
+
+		array := NewArray(storage)
+
+		n := uint64(256 * 256)
+		for i := uint64(0); i < n; i++ {
+			err := array.Append(i)
+			require.NoError(t, err)
+		}
+
+		require.Equal(t, n, array.Count())
+
+		// Remove every other elements
+		for i := uint64(0); i < array.Count(); i++ {
+			expected, err := array.Get(i)
+			require.NoError(t, err)
+
+			v, err := array.Remove(i)
+			require.NoError(t, err)
+			require.Equal(t, expected, v)
+
+			if i%8 == 0 {
+				verified, err := array.valid()
+				require.NoError(t, err)
+				require.True(t, verified)
+			}
+		}
+
+		for i, j := uint64(0), uint64(1); i < array.Count(); i, j = i+1, j+2 {
+			v, err := array.Get(i)
+			require.NoError(t, err)
+			require.Equal(t, j, v)
+		}
 	})
 }
 
 func TestSplit(t *testing.T) {
-	setThreshold(50)
-	defer func() {
-		setThreshold(1024)
-	}()
+	t.Run("leaf node as root", func(t *testing.T) {
+		storage := NewBasicStorage()
 
-	storage := NewBasicStorage()
+		array := NewArray(storage)
 
-	array := NewArray(storage)
+		n := uint64(50)
+		for i := uint64(0); i < n; i++ {
+			err := array.Append(i)
+			require.NoError(t, err)
+		}
 
-	n := uint64(50)
-	for i := uint64(0); i < n; i++ {
-		err := array.Append(i)
-		require.NoError(t, err)
-	}
+		// leaf node as root
+		require.NotNil(t, array.root)
+		require.True(t, array.root.IsLeaf())
+		require.Equal(t, uint32(50), array.root.Header().count)
+		require.Equal(t, uint32(8*50), array.root.Header().size)
+	})
+	t.Run("internal node as root", func(t *testing.T) {
+		setThreshold(50)
+		defer func() {
+			setThreshold(1024)
+		}()
 
-	// root (internal) node
-	require.NotNil(t, array.root)
-	require.False(t, array.root.IsLeaf())
-	require.Equal(t, uint32(50), array.root.header.count)
-	require.Equal(t, uint32(16*3), array.root.header.size) // 3 headers
-	require.Equal(t, 3, len(array.root.orderedHeaders))    // 3 headers
+		storage := NewBasicStorage()
 
-	for _, h := range array.root.orderedHeaders {
-		id := h.id
-		slab, found, err := storage.Retrieve(id)
-		require.NoError(t, err)
-		require.True(t, found)
-		require.False(t, slab.IsLeaf())
-	}
+		array := NewArray(storage)
+
+		n := uint64(50)
+		for i := uint64(0); i < n; i++ {
+			err := array.Append(i)
+			require.NoError(t, err)
+		}
+
+		// meta node as root
+		require.NotNil(t, array.root)
+		require.False(t, array.root.IsLeaf())
+		require.Equal(t, uint32(50), array.root.Header().count)
+		require.Equal(t, uint32(16*3), array.root.Header().size) // 3 headers
+
+		root := array.root.(*ArrayMetaDataSlab)
+		for _, h := range root.orderedHeaders {
+			id := h.id
+			slab, found, err := storage.Retrieve(id)
+			require.NoError(t, err)
+			require.True(t, found)
+			require.False(t, slab.IsLeaf())
+		}
+	})
 }
 
 func TestIterate(t *testing.T) {

28acec04907a4322a3cd58b6f620fbf1afff3999
Bastian Müller <bastian@axiomzen.co>
2021-08-01 17:01:10 -0700

    use higher value for storable flag

--- storable.go
+++ storable.go
@@ -26,7 +26,7 @@ const (
 	flagMetaDataSlab byte = 0b00000001
 	flagDataSlab     byte = 0b00000010
 	flagArray        byte = 0b00000100
-	flagStorable     byte = 0b00001000
+	flagStorable     byte = 0b10000000
 )
 
 const (

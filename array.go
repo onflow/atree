07b4214337e5b44e46de44a496a8304bcf380ece
Bastian Müller <bastian@axiomzen.co>
2021-08-10 16:33:08 -0700

    storable slab get removed by parent

--- array.go
+++ array.go
@@ -58,8 +58,8 @@ type ArrayDataSlab struct {
 	extraData *ArrayExtraData
 }
 
-func (a *ArrayDataSlab) DeepRemove(storage SlabStorage) error {
-	storage.Remove(a.ID())
+func (a *ArrayDataSlab) DeepRemove(_ SlabStorage) error {
+	// Slab will be removed by parent
 	return nil
 }
 
@@ -95,8 +95,8 @@ func (a *ArrayMetaDataSlab) StoredValue(storage SlabStorage) (Value, error) {
 	}, nil
 }
 
-func (a *ArrayMetaDataSlab) DeepRemove(storage SlabStorage) error {
-	storage.Remove(a.ID())
+func (a *ArrayMetaDataSlab) DeepRemove(_ SlabStorage) error {
+	// Slab will be removed by parent
 	return nil
 }
 

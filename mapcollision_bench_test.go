8bc3ed0f25bc9dae765ed8e7b6ffb92b8a52b6bb
Faye Amacker <33205765+fxamacker@users.noreply.github.com>
2024-06-04 15:01:38 -0500

    Update copyright notice to Flow Foundation

--- mapcollision_bench_test.go
+++ mapcollision_bench_test.go
@@ -1,7 +1,7 @@
 /*
  * Atree - Scalable Arrays and Ordered Maps
  *
- * Copyright 2022 Dapper Labs, Inc.
+ * Copyright Flow Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

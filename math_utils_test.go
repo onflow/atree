5d92fa1230da2efa1569d656a2705cd1c4687ef2
Faye Amacker <33205765+fxamacker@users.noreply.github.com>
2026-04-06 17:49:43 -0500

    Add tests

---
+++ math_utils_test.go
@@ -0,0 +1,88 @@
+/*
+ * Atree - Scalable Arrays and Ordered Maps
+ *
+ * Copyright Flow Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package atree
+
+import (
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestSafeAdd2Uint32(t *testing.T) {
+	t.Parallel()
+
+	t.Run("safe", func(t *testing.T) {
+		sum, safe := safeAdd2Uint32(1, 2)
+		require.True(t, safe)
+		require.Equal(t, uint32(3), sum)
+
+		sum, safe = safeAdd2Uint32(1, math.MaxUint32-1)
+		require.True(t, safe)
+		require.Equal(t, uint32(math.MaxUint32), sum)
+
+		sum, safe = safeAdd2Uint32(math.MaxUint32-1, 1)
+		require.True(t, safe)
+		require.Equal(t, uint32(math.MaxUint32), sum)
+	})
+
+	t.Run("not safe", func(t *testing.T) {
+		_, safe := safeAdd2Uint32(1, math.MaxUint32)
+		require.False(t, safe)
+
+		_, safe = safeAdd2Uint32(math.MaxUint32, 1)
+		require.False(t, safe)
+
+		_, safe = safeAdd2Uint32(2, math.MaxUint32-1)
+		require.False(t, safe)
+	})
+}
+
+func TestSafeAdd3Uint32(t *testing.T) {
+	t.Parallel()
+
+	t.Run("safe", func(t *testing.T) {
+		sum, safe := safeAdd3Uint32(1, 2, 3)
+		require.True(t, safe)
+		require.Equal(t, uint32(6), sum)
+
+		sum, safe = safeAdd3Uint32(1, 1, math.MaxUint32-2)
+		require.True(t, safe)
+		require.Equal(t, uint32(math.MaxUint32), sum)
+
+		sum, safe = safeAdd3Uint32(1, math.MaxUint32-2, 1)
+		require.True(t, safe)
+		require.Equal(t, uint32(math.MaxUint32), sum)
+
+		sum, safe = safeAdd3Uint32(math.MaxUint32-2, 1, 1)
+		require.True(t, safe)
+		require.Equal(t, uint32(math.MaxUint32), sum)
+	})
+
+	t.Run("not safe", func(t *testing.T) {
+		_, safe := safeAdd3Uint32(1, 1, math.MaxUint32-1)
+		require.False(t, safe)
+
+		_, safe = safeAdd3Uint32(1, math.MaxUint32-1, 1)
+		require.False(t, safe)
+
+		_, safe = safeAdd3Uint32(math.MaxUint32-1, 1, 1)
+		require.False(t, safe)
+	})
+}
